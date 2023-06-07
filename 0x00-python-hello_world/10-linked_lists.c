508fe48131f64c77305881a41f19dba0ec9d6b11
KeanuEllenAfiriyie <122979498+KeanuEllenAfiriyie@users.noreply.github.com>
2023-06-07 06:47:08 +0500

    python

--- 0x00-python-hello_world/10-linked_lists.c
+++ 0x00-python-hello_world/10-linked_lists.c
@@ -4,8 +4,7 @@
 
 /**
  * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
+
  */
 size_t print_listint(const listint_t *h)
 {
@@ -25,9 +24,7 @@ size_t print_listint(const listint_t *h)
 
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t list
- * @head: pointer to a pointer of the start of the list
- * @n: integer to be included in node
- * Return: address of the new element or NULL if it fails
+
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
@@ -44,8 +41,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 /**
  * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
+
  */
 void free_listint(listint_t *head)
 {
